75b9861485f2d77d80a51d9149da566595910b30
saurabhdaware <saurabhdaware99@gmail.com>
2022-06-11 00:51:29 +0530

    use getMergedPullRequest method

--- index.js
+++ index.js
@@ -2,33 +2,79 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fetch = require('node-fetch');
 
-function getPRNumber() {
-  const githubSquashCommit = github.context.payload.commits.find((commit) => {
-    return commit.committer.username === 'web-flow' && commit.commiter.name === 'GitHub'
-  })
+/**
+ * @Credits https://github.com/actions-ecosystem/action-get-merged-pull-request/blob/main/src/main.ts
+ * 
+ */
+async function getMergedPullRequest(
+  githubToken,
+  owner,
+  repo,
+  sha
+) {
+  const client = new github.GitHub(githubToken);
 
-  if (githubSquashCommit) {
-    const [_, prNumber] = /\(#([\d ]*?)\)/g.exec(githubSquashCommit.message);
-    if (!prNumber) return 1;
-    return prNumber;
+  const resp = await client.pulls.list({
+    owner,
+    repo,
+    sort: 'updated',
+    direction: 'desc',
+    state: 'closed',
+    per_page: 100
+  });
+
+  const pull = resp.data.find(p => p.merge_commit_sha === sha);
+  if (!pull) {
+    return null;
   }
-  
-  return 1;
+
+  return {
+    title: pull.title,
+    body: pull.body,
+    number: pull.number,
+    labels: pull.labels.map(l => l.name),
+    assignees: pull.assignees.map(a => a.login)
+  };
 }
 
-async function getLabels(prNumber) {
-  const repository = github.context.payload.repository;
-  const ENDPOINT = repository.pulls_url.replace('{/number}', `/${prNumber}`);
-  const data = await fetch(ENDPOINT).then((res) => res.json());
-  const labels = data.labels.map((label) => label.name);
-  return labels;
-}
+
+// function getPRNumber() {
+//   const githubSquashCommit = github.context.payload.commits.find((commit) => {
+//     return commit.committer.username === 'web-flow' && commit.commiter.name === 'GitHub'
+//   })
+
+//   if (githubSquashCommit) {
+//     const [_, prNumber] = /\(#([\d ]*?)\)/g.exec(githubSquashCommit.message);
+//     if (!prNumber) return 1;
+//     return prNumber;
+//   }
+
+//   return 1;
+// }
+
+// async function getLabels(prNumber) {
+//   const repository = github.context.payload.repository;
+//   const ENDPOINT = repository.pulls_url.replace('{/number}', `/${prNumber}`);
+//   const data = await fetch(ENDPOINT).then((res) => res.json());
+//   const labels = data.labels.map((label) => label.name);
+//   return labels;
+// }
 
 async function action() {
-  const mergedPRNumber = getPRNumber();
-  console.log({mergedPRNumber});
-  const labels = await getLabels(mergedPRNumber);
-  console.log({labels});
+  const pull = await getMergedPullRequest(
+    core.getInput('github_token'),
+    github.context.repo.owner,
+    github.context.repo.repo,
+    github.context.sha
+  );
+
+  console.log({pull});
+
+
+  // const mergedPRNumber = getPRNumber();
+  // console.log({mergedPRNumber});
+  // const labels = await getLabels(mergedPRNumber);
+  // console.log({labels});
 
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
